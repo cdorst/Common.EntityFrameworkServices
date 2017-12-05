d32e451b757c1be4822ec00128128d8aea088aff
Christopher Dorst <chris@dorst.email>
2017-12-05 02:38:36 -0600

    Adding UniqueListIdentifierFactory

---
+++ Common.EntityFrameworkServices/Factories/UniqueListIdentifierFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.EntityFrameworkServices.Factories
+{
+    public static class UniqueListIdentifierFactory<TRecord>
+        where TRecord : class, IUniqueListRecord
+    {
+        public static string Create<TKey>(List<TRecord> records, Func<TRecord, TKey> keySelector)
+            => string.Join(",", records.Select(keySelector));
+    }
+}
