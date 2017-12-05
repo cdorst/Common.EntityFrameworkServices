dfa9556d302aa48ccdbc59a767f507e0da794efd
Christopher Dorst <chris@dorst.email>
2017-12-04 21:00:50 -0600

    Adding UniqueList factory classes

---
+++ Common.EntityFrameworkServices/Factories/UniqueListAssociationsFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.EntityFrameworkServices.Factories
+{
+    public static class UniqueListAssociationsFactory<TRecord, TRecordListAssociation>
+        where TRecord : class, IUniqueListRecord
+        where TRecordListAssociation : IUniqueListAssociation<TRecord>
+    {
+        public static List<TRecordListAssociation> Create(List<TRecord> records)
+        {
+            var associations = Activator.CreateInstance<List<TRecordListAssociation>>();
+            if (records == null) return associations;
+            foreach (var record in records)
+            {
+                var association = Activator.CreateInstance<TRecordListAssociation>();
+                association.SetRecord(record);
+                associations.Add(association);
+            }
+            return associations;
+        }
+    }
+}
---
+++ Common.EntityFrameworkServices/Factories/UniqueListFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.EntityFrameworkServices.Factories
+{
+    public static class UniqueListFactory<TRecord, TRecordList, TRecordListAssociation>
+        where TRecord : class, IUniqueListRecord
+        where TRecordList : class, IUniqueList<TRecord, TRecordListAssociation>
+        where TRecordListAssociation : IUniqueListAssociation<TRecord>
+    {
+        public static TRecordList Create(List<TRecord> records)
+        {
+            var instance = Activator.CreateInstance<TRecordList>();
+            instance.SetRecords(records);
+            return instance;
+        }
+    }
+}
