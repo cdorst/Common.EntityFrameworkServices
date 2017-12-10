d88f80b6d804e92a7278a8b1c277a48d2c0d6819
Christopher Dorst <chris@dorst.email>
2017-12-09 18:22:53 -0600

    Adding UniqueListExtensions.WithRecords

---
+++ Common.EntityFrameworkServices/UniqueListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.EntityFrameworkServices
+{
+    public static class UniqueListExtensions
+    {
+        public static IUniqueList<TRecord, TRecordListAssociation> WithRecords<TRecord, TRecordListAssociation>(this IUniqueList<TRecord, TRecordListAssociation> list, List<TRecord> records)
+            where TRecord : class, IUniqueListRecord
+            where TRecordListAssociation : IUniqueListAssociation<TRecord>
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            list.SetRecords(records);
+            return list;
+        }
+    }
+}
